06652b80eb5f75e5b31be2dfda198f56efbaa4b4
Bushmakin Mikhail <mbushmakin@vsegda-da.com>
2022-11-01 22:31:00 +0400

    Task #2 SarumanAndLanisters

---
+++ src/main/java/SarumanAndLanisters.java
@@ -0,0 +1,12 @@
+public class SarumanAndLanisters {
+
+    public static void main(String[] args) {
+
+        final int warriorsAtLanisters = 2500;
+        System.out.println("Пехотинцы Ланистеров: " + warriorsAtLanisters);
+        final int orksAtSaruman = 3000;
+        System.out.println("Орки Сарумана: " + orksAtSaruman);
+        final int armyLanistersAndSaruman = warriorsAtLanisters + orksAtSaruman;
+        System.out.println("Численность войска Ланистеров и Сарумана: " + armyLanistersAndSaruman);
+    }
+}
